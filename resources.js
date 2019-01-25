d8cbc5a48193f8183df09d34cb859d603a40a924
Ashi Krishnan <queerviolet@github.com>
2019-01-25 14:09:49 -0600

    silent speech

--- resources.js
+++ resources.js
@@ -1,5 +1,7 @@
 import res0 from "./res/the-recursive-mind.jpg"
-import res1 from "./res/language-tree-mina-sundberg.jpg"
+import res1 from "./res/phonological-loop.jpg"
+import res2 from "./res/mit-silent-speech.jpg"
+import res3 from "./res/language-tree-mina-sundberg.jpg"
 export default {
-    "the-recursive-mind.jpg": res0,"language-tree-mina-sundberg.jpg": res1
+    "the-recursive-mind.jpg": res0,"phonological-loop.jpg": res1,"mit-silent-speech.jpg": res2,"language-tree-mina-sundberg.jpg": res3
   }
