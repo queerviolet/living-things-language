68e2ae996e971b116c7a097eb95564602250c088
Ashi Krishnan <queerviolet@github.com>
2019-01-28 16:36:47 -0500

    Cerebellum and origins of language.

--- resources.js
+++ resources.js
@@ -4,7 +4,10 @@ import res2 from "./res/mit-silent-speech.jpg"
 import res3 from "./res/phonological-loop.jpg"
 import res4 from "./res/gray-cerebellum-pink.png"
 import res5 from "./res/sobo-cerebellum-pink.png"
-import res6 from "./res/language-tree-mina-sundberg.jpg"
+import res6 from "./res/gray-larynx-anterior.png"
+import res7 from "./res/gray-larynx-front.png"
+import res8 from "./res/gray-larynx-saggital.png"
+import res9 from "./res/language-tree-mina-sundberg.jpg"
 export default {
-    "the-recursive-mind.jpg": res0,"phonological-loop.jpg": res1,"mit-silent-speech.jpg": res2,"phonological-loop.jpg": res3,"gray-cerebellum-pink.png": res4,"sobo-cerebellum-pink.png": res5,"language-tree-mina-sundberg.jpg": res6
+    "the-recursive-mind.jpg": res0,"phonological-loop.jpg": res1,"mit-silent-speech.jpg": res2,"phonological-loop.jpg": res3,"gray-cerebellum-pink.png": res4,"sobo-cerebellum-pink.png": res5,"gray-larynx-anterior.png": res6,"gray-larynx-front.png": res7,"gray-larynx-saggital.png": res8,"language-tree-mina-sundberg.jpg": res9
   }
